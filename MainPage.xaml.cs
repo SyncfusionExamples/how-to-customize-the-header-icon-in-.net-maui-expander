1500b5e5d1529e2306c4d702adc3fdbe472c4582
AnanthaLakshmiKannan <ananthalakshmi.k@syncfusion.com>
2023-12-20 16:16:00 +0530

    how-to-customize-the-header-icon-in-.net-maui-expander

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpanderMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExpanderMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
