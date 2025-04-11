fdea1f00e8762e1ada057421abd0115ee513fb6f
SethupathyD <sethupathy.d@syncfusion.com>
2025-04-11 16:15:28 +0530

    Code updated.

---
+++ ExpanderMaui/ExpanderMaui/Helper/ExpanderIconConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace ExpanderMaui
+{
+    public class ExpanderIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return "arrowdown.png";
+            else
+                return "arrowup.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
